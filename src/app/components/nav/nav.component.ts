3ac9352ce2c80e4823f549dd9bbe84a47679eee0
vale2729 <valentinaaj2729@gmail.com>
2021-04-20 23:34:24 -0500

    logo

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Producto } from 'src/app/interfaces/producto';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
 
-  constructor(private title: Title) { }
+export class NavComponent implements OnInit {
+  
+  constructor(private title: Title, private log : LoginService) { }
 
   ngOnInit(): void {
     this.title.setTitle('El colombianito');
