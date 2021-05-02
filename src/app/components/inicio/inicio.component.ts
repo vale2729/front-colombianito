97a26ae85f05a5fc969dc834d6771cddefe31de4
EvidaliaP <82007769+EvidaliaP@users.noreply.github.com>
2021-05-01 23:05:22 -0500

    Carrucel y contactenos
    se realizo el carrusel y contactenos

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  images = [`ComidaCarrucel1.jpg`, `ComidaCarrucel2.jpeg`, `ComidaCarrucel3.jpg`].map((n) => `/assets/Carrucel/${n}`);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 1000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
   }
+  ngOnInit(): void {
 
+  }
+  
 }
